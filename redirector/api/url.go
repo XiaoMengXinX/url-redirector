b7822149d53a5142a750f9bc36c445d46e526874
lz已经是条咸鱼了 <xibaole2333@gmail.com>
2022-03-04 13:23:16 +0800

    upload code

--- redirector/api/url.go
+++ redirector/api/url.go
@@ -9,29 +9,19 @@ import (
 
 var routes map[string]string
 
-type resData struct {
-	Token string `json:"token"`
-	Error string `json:"error"`
-}
-
 func init() {
 	_ = json.Unmarshal(route.Data, &routes)
 }
 
 func UrlHandler(w http.ResponseWriter, r *http.Request) {
 	if len(r.URL.Path) <= 1 {
-		_, _ = fmt.Fprintf(w, responseJson(resData{Error: "Invaid short name"}))
+		_, _ = fmt.Fprintf(w, "Invaid short name")
 		return
 	}
 	if url := routes[r.URL.Path[1:]]; url != "" {
 		http.Redirect(w, r, url, http.StatusMovedPermanently)
 	} else {
-		_, _ = fmt.Fprintf(w, responseJson(resData{Error: "Invaid short name"}))
+		_, _ = fmt.Fprintf(w, "Invaid short name")
 	}
 	return
-}
-
-func responseJson(r resData) string {
-	b, _ := json.Marshal(r)
-	return string(b)
 }
